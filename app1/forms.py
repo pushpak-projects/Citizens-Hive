432589fcf9f50906827459fdf26032f3dca77e93
Pushpak Bhattacharyya <pushpakbhattacharyya5@gmail.com>
2021-04-30 04:20:18 -0700

    All code changes

---
+++ app1/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
+
+PAYMENT_CHOICES = (
+    ('S', 'Stripe')
+)
+
+class CheckoutForm(forms.Form):
+    billing_address = forms.CharField(required=False)
+    billing_address2 = forms.CharField(required=False)
+    billing_country = CountryField(blank_label='(select country)').formfield(
+        required=False,
+        widget=CountrySelectWidget(attrs={
+            'class': 'custom-select d-block w-100',
+        }))
+    billing_zip = forms.CharField(required=False)
+
+    # same_billing_address = forms.BooleanField(required=False)
+    # set_default_shipping = forms.BooleanField(required=False)
+    # use_default_shipping = forms.BooleanField(required=False)
+    set_default_billing = forms.BooleanField(required=False)
+    use_default_billing = forms.BooleanField(required=False)
+
+    # payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+class PaymentForm(forms.Form):
+    stripeToken = forms.CharField(required=False)
+    save = forms.BooleanField(required=False)
+    use_default = forms.BooleanField(required=False)
